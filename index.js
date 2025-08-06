290ee66d5ff91bdfa8f65147c8f5fccca823d86b
Jumana <jumthaibi@gmail.com>
2025-08-06 12:21:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const display = document.getElementById("display")
-
+        let val = 0;
 function add_to_dispaly(input){
     display.value += input;
 }
 
-function clear_display(){
+function add_to_value(input){
+    val += input;
+}
+
+function clear_val(){
     display.value = " ";
+    val = " ";
 }
 
 function calculate(){
     try {
-        display.value = eval(display.value);
+        display.value = eval(val);
     } catch (e) {
         display.value = "Error";
     }
